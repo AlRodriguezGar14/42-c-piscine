b9e501e12e95c7e648e78756defbcc0a04fd3b6c
Alberto Rodriguez <alberrod@c2r1s4.42urduliz.com>
2023-10-12 20:01:58 +0200

    removed non permitted funcion. Added line break using the required function two times

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,11 +6,10 @@
 /*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 16:56:08 by alberrod          #+#    #+#             */
-/*   Updated: 2023/10/12 18:44:57 by alberrod         ###   ########.fr       */
+/*   Updated: 2023/10/12 20:00:21 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -22,8 +21,7 @@ int	main(void)
 {
 	char	character_c;
 
-	character_c = 'c';
-	ft_putchar(character_c);
-	printf("\n");
+	ft_putchar('c');
+	ft_putchar('\n');
 	return (0);
 }
