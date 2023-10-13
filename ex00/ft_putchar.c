519a144f5c09f67531ad8de44fbe908cae82f859
Alberto Rodriguez <alberrod@c2r1s1.42urduliz.com>
2023-10-13 09:30:28 +0200

    main() removed from ex00

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 16:56:08 by alberrod          #+#    #+#             */
-/*   Updated: 2023/10/12 20:00:21 by alberrod         ###   ########.fr       */
+/*   Updated: 2023/10/13 09:25:23 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,4 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-int	main(void)
-{
-	char	character_c;
-
-	ft_putchar('c');
-	ft_putchar('\n');
-	return (0);
 }
