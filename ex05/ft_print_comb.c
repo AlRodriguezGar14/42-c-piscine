1d8dd4bf68fb03ac9c26a6b7b1ec3b9fc31ee2d3
Alberto Rodriguez <alberrod@c1r1s4.42urduliz.com>
2023-10-16 12:27:36 +0200

    ex05 done. Pending: Remove the main function

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,20 +6,30 @@
 /*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:28:53 by alberrod          #+#    #+#             */
-/*   Updated: 2023/10/16 11:46:58 by alberrod         ###   ########.fr       */
+/*   Updated: 2023/10/16 12:26:03 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_print_char(char hundreth, char decimal, char numeral)
+{
+	write(1, &hundreth, 1);
+	write(1, &decimal, 1);
+	write(1, &numeral, 1);
+	if (!(numeral == '9' && decimal == '8' && hundreth == '7'))
+	{
+		write(1, ", ", 2);
+	}
+}
+
 void	ft_print_comb(void)
 {
-	char numeral;
-	char decimal;
-	char hundreth;
+	char	numeral;
+	char	decimal;
+	char	hundreth;
 
-	hundreth  = '0';
-
+	hundreth = '0';
 	while (hundreth != decimal)
 	{
 		decimal = hundreth + 1;
@@ -28,25 +38,16 @@ void	ft_print_comb(void)
 			numeral = decimal + 1;
 			while (numeral <= '9')
 			{
-				write(1, &hundreth, 1);
-				write(1, &decimal, 1);
-				write(1, &numeral, 1);
-				if (!(numeral  == '9' && decimal == '8' && hundreth == '7'))
-				{
-					write(1, ", ", 2);
-				}
-
+				ft_print_char(hundreth, decimal, numeral);
 				numeral++;
 			}
 			decimal++;
 		}
 		hundreth++;
 	}
-
 }
 
-
-int	main()
+int	main(void)
 {
 	ft_print_comb();
 	return (0);
