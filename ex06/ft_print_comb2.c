e3b9041d9cf16490c06eca8d594134ba465b5224
Alberto Rodriguez <alberrod@c1r2s1.42urduliz.com>
2023-10-17 17:21:15 +0200

    ex06 done. Norminette pass

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:58:03 by alberrod          #+#    #+#             */
-/*   Updated: 2023/10/17 15:44:12 by alberrod         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:20:28 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_print_comb2(void)
 {
-	int	a;
-	int	b;
+	int		a;
+	int		b;
 	char	printer[5];
-	int		index;
 
 	printer[2] = ' ';
 	a = 0;
-	while(a < 99)
+	while (a < 99)
 	{
 		printer[0] = a / 10 + '0';
 		printer[1] = a % 10 + '0';
@@ -31,17 +29,9 @@ void	ft_print_comb2(void)
 		{
 			printer[3] = b / 10 + '0';
 			printer[4] = b % 10 + '0';
-
-			index = 0;
-			while(index < 5)
-			{
-				write(1, &printer[index], 1);
-				index++;
-			}
+			write(1, printer, 5);
 			if (a < 98)
-			{
 				write(1, ", ", 2);
-			}
 			b++;
 		}
 		a++;
@@ -51,5 +41,5 @@ void	ft_print_comb2(void)
 int	main(void)
 {
 	ft_print_comb2();
-	return 0;
+	return (0);
 }
