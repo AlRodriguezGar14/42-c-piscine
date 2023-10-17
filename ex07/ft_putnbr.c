f23ba8bd86b40a472bdc79da75ea5a9bf6e9a1e5
Alberto Rodriguez <alberrod@c1r2s1.42urduliz.com>
2023-10-17 12:33:19 +0200

    exercise ex07 working

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 19:17:56 by alberrod          #+#    #+#             */
-/*   Updated: 2023/10/16 22:46:24 by alberrod         ###   ########.fr       */
+/*   Updated: 2023/10/17 12:30:27 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,33 @@
 
 void	ft_putnbr(int nb)
 {
-	int n;
-	int tmp;
-	char printable;
+	int		n;
+	int		tmp;
+	char	printable;
 
 	n = 10;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		write(1, "-", 1);
+	}
 	tmp = nb;
-
-	// TODO: Convert nb to positive if negative and print - before any operation
-	// nb = nb * -1
-	// write(1, '-', 1)
-	
-	// This tells the function how many loops to execute
 	while (tmp > 90)
 	{
 		tmp = tmp / 10;
 		n = n * 10;
 	}
-
-	// This is the conversor
 	while (n > 0)
 	{
 		printable = (nb / n) + '0';
 		write(1, &printable, 1);
-
-		// Remove the already printed value
 		nb = nb % n;
-
-		// Reduce the loop by one
 		n = n / 10;
 	}
 }
 
-int	main(void)
-{
-	ft_putnbr(4242);
-	return (0);
-}
+/* int	main(void) */
+/* { */
+/* 	ft_putnbr(-444); */
+/* 	return (0); */
+/* } */
